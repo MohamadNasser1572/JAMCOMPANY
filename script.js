bc21148dd3956446dad92806b14b08b00d62e18c
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-07-26 15:19:19 +0300

    remove commits in scripts.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// script.js
-
 document.addEventListener("DOMContentLoaded", () => {
-  // Placeholder for any future JavaScript functionality
   console.log("JAM Retail Company page loaded");
 });
